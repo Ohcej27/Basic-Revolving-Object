8fd4ad8f5c40b17c6524252b08d4143b7e328dce
Ohcej27 <baltazarjecho27@gmail.com>
2022-10-11 18:18:32 +0400

    First Commit
    First date: 11/10/2022

---
+++ revolve.js
@@ -0,0 +1,8 @@
+
+        function startButton() {
+            document.getElementById("monoLogo").style.animationPlayState = "running";
+        }
+
+        function stopButton() {
+            document.getElementById("monoLogo").style.animationPlayState = "paused";
+        }
